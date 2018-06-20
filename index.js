6f33352e7c7c826d99f3a5b5271a8aa56a388e41
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-06-20 16:35:57 +0200

    :lipstick: Order Imports

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda.json'
 import modeler from 'bpmn-js/lib/Modeler';
 import viewer from 'bpmn-js/lib/Viewer';
 
-import ZoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
-import MoveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
 import MiniMap from 'diagram-js-minimap';
+import MoveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
+import ZoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
+
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 import "diagram-js/assets/diagram-js.css";
 import "./src/diagram-js-minimap.css";
