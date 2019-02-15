c1f2f5b568e6784950a4aa0693c0456f95428f1a
Vyperus <bastian.schnittger@5minds.de>
2019-02-15 15:57:06 +0100

    :sparkles: Add LintModule to Custom Bundle

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import viewer from 'bpmn-js/lib/Viewer';
 import MiniMap from 'diagram-js-minimap';
 import MoveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
 import ZoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
+import lintModule from 'bpmn-js-bpmnlint';
 
 import resizeAllModule from './src/resize-all-rules';
 
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 import "diagram-js/assets/diagram-js.css";
+import "bpmn-js-bpmnlint/dist/assets/css/bpmn-js-bpmnlint.css";
 import "./src/diagram-js-minimap.css";
 
 export {
@@ -19,5 +21,6 @@ export {
  MoveCanvasModule,
  viewer,
  ZoomScrollModule,
- resizeAllModule
+ resizeAllModule,
+ lintModule,
 }
