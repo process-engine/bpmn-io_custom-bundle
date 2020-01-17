709051a1094ca921675a98716edb63cd6beb0d8e
Steffen <Steffen.knaup@5minds.de>
2020-01-17 09:32:19 +0100

    :rewind: Undo Updating bpmn-js

--- src/resize-all-rules/ResizeAllRules.js
+++ src/resize-all-rules/ResizeAllRules.js
@@ -16,16 +16,15 @@ ResizeAllRules.prototype.init = function() {
 
   this.addRule('shape.resize', 1500, function(event) {
 
-    const shapeIsResizable = event.shape.type.includes('Task')
+    const shapeIsTask = event.shape.type.includes('Task')
                      || event.shape.type === 'bpmn:SubProcess'
                      || event.shape.type === 'bpmn:CallActivity'
                      || event.shape.type === 'bpmn:Participant'
                      || event.shape.type === 'bpmn:Lane'
                      || event.shape.type === 'bpmn:LaneSet'
-                     || event.shape.type === 'bpmn:TextAnnotation'
-                     || event.shape.type === 'bpmn:Group';
+                     || event.shape.type === 'bpmn:TextAnnotation';
 
-    if (shapeIsResizable){
+    if (shapeIsTask){
       return true;
     }
 
